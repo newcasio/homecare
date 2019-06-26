2780921d90e3003abbf783c71fc26f6004d9d9c0
Brad <braddong@hotmail.com>
2019-06-26 21:16:30 +1000

    showtask alignment start

--- src/components/tabs/ShowTasks.jsx
+++ src/components/tabs/ShowTasks.jsx
@@ -10,13 +10,13 @@ import avatarPic from "../../public/dog.png";
 
 function ShowTasks({ data }) {
   const container = {
-    display: "flex",
-    padding: "15px 0px",
-    borderBottom: "1px #E4E6E8 solid"
+    padding: "15px 0px"
   };
 
   const eventStyle = {
-    padding: "0px, 10px, 10px, 10px"
+    padding: "0px, 10px, 10px, 10px",
+    display: "grid",
+    gridTemplateColumns: "5% 30% 5% 60% "
   };
 
   return (
@@ -25,35 +25,36 @@ function ShowTasks({ data }) {
         <div key={ev.id} style={container}>
           <Avatar className="avatar" alt="profile pic" src={avatarPic} />
 
-          <Grid style={eventStyle}>
-            <Grid xs={12}>
+          <div>
+            <div>
               <h4 style={{ color: "#0E8A90", margin: "0px" }}>{ev.name}</h4>
-            </Grid>
+            </div>
 
-            <Grid xs={1}>
-              <SvgIcon>
-                <path d="M19,4H18V2H16V4H8V2H6V4H5C3.89,4 3,4.9 3,6V20A2,2 0 0,0 5,22H19A2,2 0 0,0 21,20V6A2,2 0 0,0 19,4M19,20H5V10H19V20M19,8H5V6H19V8Z" />
-              </SvgIcon>
-            </Grid>
+            <div style={eventStyle}>
+              <div>
+                <SvgIcon>
+                  <path d="M19,4H18V2H16V4H8V2H6V4H5C3.89,4 3,4.9 3,6V20A2,2 0 0,0 5,22H19A2,2 0 0,0 21,20V6A2,2 0 0,0 19,4M19,20H5V10H19V20M19,8H5V6H19V8Z" />
+                </SvgIcon>
+              </div>
 
-            <Grid xs={5}>
-              <span style={{ paddingRight: "8px" }}>{ev.date}</span>
-            </Grid>
+              <div>
+                <span style={{ paddingRight: "8px" }}>{ev.date}</span>
+              </div>
 
-            <Grid xs={1}>
-              <SvgIcon>
-                <path d="M12,20A7,7 0 0,1 5,13A7,7 0 0,1 12,6A7,7 0 0,1 19,13A7,7 0 0,1 12,20M12,4A9,9 0 0,0 3,13A9,9 0 0,0 12,22A9,9 0 0,0 21,13A9,9 0 0,0 12,4M12.5,8H11V14L15.75,16.85L16.5,15.62L12.5,13.25V8M7.88,3.39L6.6,1.86L2,5.71L3.29,7.24L7.88,3.39M22,5.72L17.4,1.86L16.11,3.39L20.71,7.25L22,5.72Z" />
-              </SvgIcon>
-            </Grid>
+              <div>
+                <SvgIcon>
+                  <path d="M12,20A7,7 0 0,1 5,13A7,7 0 0,1 12,6A7,7 0 0,1 19,13A7,7 0 0,1 12,20M12,4A9,9 0 0,0 3,13A9,9 0 0,0 12,22A9,9 0 0,0 21,13A9,9 0 0,0 12,4M12.5,8H11V14L15.75,16.85L16.5,15.62L12.5,13.25V8M7.88,3.39L6.6,1.86L2,5.71L3.29,7.24L7.88,3.39M22,5.72L17.4,1.86L16.11,3.39L20.71,7.25L22,5.72Z" />
+                </SvgIcon>
+              </div>
 
-            <Grid xs={5}>
-              <span>{ev.time}</span>
-            </Grid>
-
-            <Grid xs={12}>
+              <div xs={4}>
+                <span>{ev.time}</span>
+              </div>
+            </div>
+            <div xs={12}>
               <Status status={ev.status} />
-            </Grid>
-          </Grid>
+            </div>
+          </div>
         </div>
       ))}
     </div>
